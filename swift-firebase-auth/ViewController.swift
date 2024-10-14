fe5734d5d7f0d02f38c60ce1ba7442bf47300e3c
Edson Brandon <edsonbrandon8@gmail.com>
2024-10-14 10:50:52 -0300

    Initial Commit

---
+++ swift-firebase-auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-firebase-auth
+//
+//  Created by Edson Brandon on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
